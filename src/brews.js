3e51a6fcdf6b36c39a49053199db4095aec50fa4
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-10 12:48:10 +0300

    Add my coffee brews to the menu

--- src/brews.js
+++ src/brews.js
@@ -21,3 +21,64 @@ const coffeeBrewMenu = (brew_image, brew_name, brew_desc, brew_price,) => {
   return coffeeBrews;
 };
 
+const coffeeMenu = () => {
+  const menu = document.createElement('div');
+
+    menu.appendChild(coffeeBrewMenu(
+      './dist/images/cappuccino.jpg',
+      'Cappuccino',
+      'Traditionally prepared with equal parts double espresso, steamed milk, and steamed milk foam',
+      'ksh. 1,350'
+      ),
+    );
+
+    menu.appendChild(coffeeBrewMenu(
+      './dist/images/cappuccino.jpg',
+      'Cappuccino',
+      'Traditionally prepared with equal parts double espresso, steamed milk, and steamed milk foam',
+      'ksh. 1,350'
+       ),
+    );
+
+    menu.appendChild(coffeeBrewMenu(
+      './dist/images/caramel-macchiato.jpg',
+      'Caramel Macchiato',
+      'Made with vanilla syrup, steamed milk, espresso and caramel sauce',
+      'ksh. 1,150'
+      ),
+    );
+
+    menu.appendChild(coffeeBrewMenu(
+      './dist/images/french-vanilla.jpg',
+      'French Vanilla',
+      'Made with whole milk and granulated sugar, and heated over medium heat until steaming',
+      'ksh. 1,475'
+      ),
+    );
+
+    menu.appendChild(coffeeBrewMenu(
+      './dist/images/hot-chocolate.jpg',
+      'Hot Chocolate',
+      'Made with combined milk, cocoa powder, and semi sweet chocolate, and brought to a simmer',
+      'ksh. 1,600'
+      ),
+    );
+
+    menu.appendChild(coffeeBrewMenu(
+      './dist/images/latte.jpg',
+      'Italian latte',
+      'The hot milk and creamy milk foam are imbued with the delicious flavour of coffee',
+      'ksh. 1,800'
+      ),
+    );
+
+    menu.appendChild(coffeeBrewMenu(
+      './dist/images/mocha.jpg',
+      'Cappuccino',
+      'Is a chocolate-flavoured warm beverage that is a variant of a caffè latte',
+      'ksh. 1,350'
+      ),
+    );
+  
+  return menu;
+};
