c5120e3877e0c19dafca18796541ed6555671f53
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-11 13:56:51 +0300

    refactor navigation tabs on header.js file

--- src/header.js
+++ src/header.js
@@ -1,6 +1,10 @@
+export const homeLink = document.createElement('a'); 
+export const menuLink = document.createElement('a');
+export const contactLink = document.createElement('a');
+
 const header = () => {
   const navigation = document.createElement('div');
-  navigation.classList.add('d-flex justify-content-between');
+  navigation.className = 'd-flex justify-content-between';
 
   const img_div = document.createElement('div');
   const logo = document.createElement('img');
@@ -10,29 +14,24 @@ const header = () => {
   navigation.append(img_div);
 
   const links = document.createElement('ul');
-  navigation.append(links);
-  const list_1 = document.createElement('li');
-  const tab_1 = document.createElement('button');
-  tab_1.textContent = 'Home';
-  tab_1.className = 'nav-link';
-  list_1.append(tab_1);
-  links.append(list_1);
 
-  const list_2 = document.createElement('li');
-  const tab_2 = document.createElement('button');
-  tab_2.textContent = 'Brews';
-  tab_2.className = 'nav-link';
-  list_2.append(tab_2);
-  links.append(list_2);
+  homeLink.textContent = 'Home';
+  const homeItem = document.createElement('li');
+  homeItem.appendChild(homeLink);
+  links.appendChild(homeItem);
+  
 
-  const list_3 = document.createElement('li');
-  const tab_3 = document.createElement('button');
-  tab_3.textContent = 'Contact Us';
-  tab_3.className = 'nav-link';
-  list_3.append(tab_3);
-  links.append(list_3);
+  menuLink.textContent = 'Menu';
+  const menuItem = document.createElement('li');
+  menuItem.appendChild(menuLink);
+  links.appendChild(menuItem);
 
+  contactLink.textContent = 'Contact Us';
+  const contactItem = document.createElement('li');
+  contactItem.appendChild(contactLink);
+  links.appendChild(contactItem);
 
- return navigation;
+  navigation.appendChild(links);
+  return navigation;
 };
 export default header;
