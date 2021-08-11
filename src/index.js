212b28e2c342ba19c6325eba9d7451c36f65748c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-11 16:26:36 +0300

    Remove duplication declaration of identifier header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+import { homeLink, menuLink, contactLink } from "./header.js";
 import header from "./header.js";
 import about from "./home.js";
 import showMenu from "./brews.js";
 import contact_us from "./contact.js";
 
-header();
-about();
-showMenu();
-contact_us();
+const navSection = header();
+const homePage = about();
+const menuPage = showMenu();
+const contactPage = contact_us();
+
+const content = document.querySelector('#content');
+content.append(navSection)
+
+const viewTab = (tab) => {
+  content.innerHTML = '';
+  content.append(navSection, tab);
+}
+
+homeLink.addEventListener('click', ()=>{viewTab(homePage)});
+menuLink.addEventListener('click', ()=>{viewTab(menuPage)});
+contactLink.addEventListener('click', ()=>{viewTab(contactPage)});
