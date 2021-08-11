bd25bbe1fdd1b26d8827a25576e8a5fd58dc3302
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-11 18:06:40 +0300

    Style home page

--- src/home.js
+++ src/home.js
@@ -9,16 +9,18 @@ const about =()=> {
   section.append(heading);
 
   const large_div = document.createElement('div');
-  large_div.className = 'large_div';
+  large_div.className = 'd-flex flex-row';
 
   const left_div = document.createElement('div');
   const welcomeImage = document.createElement('img');
+  welcomeImage.className = 'big-image';
   welcomeImage.setAttribute("src", "./images/Coffee.jpeg");
   left_div.append(welcomeImage);
   large_div.append(left_div);
 
   const right_div = document.createElement('div');
   right_div.textContent = "At Mbegera's, we take great care to serve quality, great-tasting menu items to our customers each and every time they visit our restaurants. We understand that each of our customers has individual needs and considerations when choosing a place to eat or drink outside their home, especially those customers with food allergies. As part of our commitment to you, we provide the most current ingredient information available from our food suppliers for the eight most common allergens";
+  right_div.className = 'paragraph'
   large_div.append(right_div);
   
   section.append(large_div);
